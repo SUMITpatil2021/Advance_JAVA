c4f59238a6f926c8174a6947e475b6b20cef4ac5
SUMITpatil2021 <sumit.patil2021@gmail.com>
2023-12-29 00:09:18 +0530

    JDBC databse connection first commit

---
+++ App/src/TestApplication.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestApplication {
+
+	public static void main(String[] args) {
+	
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			String jdbcurl="jdbc:mysql://localhost:3306/knowit";
+			Connection con=DriverManager.getConnection(jdbcurl,"root","sumit");
+			System.out.println("Connection Established succesfully");
+		}
+		catch(ClassNotFoundException e)
+		{
+			System.out.println("Driver Not Registered");
+		}
+		catch(SQLException e)
+		{
+			System.out.println("Connection failed ");
+		}
+	}
+
+}
